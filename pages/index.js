344c4b46d3c65446dfc81aac2fe88fb1cadd798d
MrTeeSG <ductieu.vrg@gmail.com>
2024-12-22 16:48:36 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-import VRGLayout from '../components/layout/VRGLayout'
-
-export default function Home() {
-  return (
-    <VRGLayout>
-      <h1>Chào mừng đến với Hệ thống Quản lý Kỹ thuật VRG</h1>
-    </VRGLayout>
-  )
-}
