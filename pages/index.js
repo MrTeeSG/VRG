3de4ba2c8148329f040a1514fef20eafc92c2047
MrTeeSG <ductieu.vrg@gmail.com>
2024-12-22 14:56:17 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import VRGLayout from '../components/layout/VRGLayout'
+
+export default function Home() {
+  return (
+    <VRGLayout>
+      <h1>Chào mừng đến với Hệ thống Quản lý Kỹ thuật VRG</h1>
+    </VRGLayout>
+  )
+}
